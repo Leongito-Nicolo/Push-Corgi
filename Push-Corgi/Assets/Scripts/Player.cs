a7cb9ab3e2f66355d24aa180af336b7333a3c833
Leongito Nicolò <leongito.nicolo@gmail.com>
2025-10-28 12:37:21 +0100

    NL - velocity

--- Push-Corgi/Assets/Scripts/Player.cs
+++ Push-Corgi/Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private float velocity = 5f;
+    private float velocity = 9f;
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Win"))
