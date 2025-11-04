b4f5ba0ae245520c47070abb2d9ffbfcd7d8e322
Leongito Nicolò <leongito.nicolo@gmail.com>
2025-11-04 20:15:55 +0100

    NL - fix script

--- Push-Corgi/Assets/Scripts/SkinUIManager.cs
+++ Push-Corgi/Assets/Scripts/SkinUIManager.cs
@@ -6,12 +6,15 @@ using UnityEngine.UI;
 public class SkinUIManager : MonoBehaviour
 {
     private int currentSkin = 0;
-    [SerializeField] private GameObject skinImage;
+    [SerializeField] private GameObject skinPanel;
     [SerializeField] private List<Sprite> skins;
     [SerializeField] private TMP_Text skinCounter;
 
+    private Image skinImage;
+
     void Awake()
     {
+        skinImage = skinPanel.GetComponent<Image>();
         skinCounter.text = $"{currentSkin + 1}/{skins.Count}";
     }
 
@@ -32,7 +35,7 @@ public class SkinUIManager : MonoBehaviour
         }
 
         currentSkin = (skins.Count + currentSkin) % skins.Count;
-        skinImage.GetComponent<Image>().sprite = skins[currentSkin];
+        skinImage.sprite = skins[currentSkin];
         skinCounter.text = $"{currentSkin + 1}/{skins.Count}";
         Debug.Log(currentSkin);
     }
